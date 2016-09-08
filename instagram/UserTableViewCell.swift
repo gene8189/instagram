c83b3a2128341c9def3cd713a4575415116345cd
Keith Piong <keith@Keiths-MacBook-Pro.local>
2016-09-08 19:41:40 +0800

    feedStoryBoardEdit

---
+++ instagram/UserTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  UserTableViewCell.swift
+//  instagram
+//
+//  Created by Keith Piong on 08/09/2016.
+//  Copyright © 2016 Tan Yee Gene. All rights reserved.
+//
+
+import UIKit
+
+
+
+class UserTableViewCell: UITableViewCell {
+ 
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBAction func optionsButton(sender: AnyObject) {
+        
+    }
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
