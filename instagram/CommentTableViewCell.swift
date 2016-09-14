ef8af56eaee74b7a9dd1dc486f279ca5569b7c28
Keith Piong <keith@Keiths-MacBook-Pro.local>
2016-09-14 16:26:58 +0800

    like commit

--- instagram/CommentTableViewCell.swift
+++ instagram/CommentTableViewCell.swift
@@ -10,14 +10,18 @@ import UIKit
 import FirebaseDatabase
 import FirebaseAuth
 
-class CommentTableViewCell: UITableViewCell {
 
+
+class CommentTableViewCell: UITableViewCell {
+    
     @IBOutlet weak var comment: UIButton!
     @IBOutlet weak var like: UIButton!
     @IBOutlet weak var share: UIButton!
     @IBOutlet weak var captionTextView: UITextView!
     @IBOutlet weak var commentsTextView: UITextView!
     
+    var likesArray = [String]()
+    var postUid: String!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -28,26 +32,46 @@ class CommentTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
-    }
-    @IBAction func likeButtonPressed(sender: UIButton) {
         
+    }
+
+    
+    @IBAction func likeButtonPressed(sender: UIButton) {
         let uid = FIRAuth.auth()!.currentUser!.uid
         
         let likesDict = ["created_at" : NSDate().timeIntervalSince1970,
                          "userUID" : uid]
-        
-        let likesRef =  DataService.rootRef.child("Usernames").child(uid).child("Photo Liked").childByAutoId() //generate auto id
-        
-        likesRef.setValue(likesDict)
-        
-//        DataService.postRef.child(uid).child("UsersWhoLiked").updateChildValues([likesRef.key: true])
-//        DataService.postRef.child(uid).observeEventType(FIRDataEventType.ChildAdded, withBlock: {(snapshot) in
-//            let)
-        
-//         DataServices.usersRef.child(User.currentUserUid).child("tweets").observeEventType(FIRDataEventType.ChildAdded, withBlock:{(snapshot) in
-//                    let chatKey = snapshot.key
+        print(self.postUid)
+       
         
         
-    }
+        let likeRef = DataService.postRef.child(self.postUid).child("UsersWhoLiked")
+        let userLiked = [uid : true]
+        let photolikedRef = DataService.usernameRef.child(uid).child("PhotoLiked")
+        let photoRef = [self.postUid : true]
+        likeRef.setValue(userLiked)
+        photolikedRef.updateChildValues(photoRef)
+        
+//        let likesRef =  DataService.rootRef.child("Usernames").child(uid).child("Photos Liked").childByAutoId() //generate auto id
+//        
+//        likesRef.setValue(likesDict)
+//        
+//            
+//        })
+//        let likesRef =  DataService.rootRef.child("Usernames").child(uid).child("Photos Liked").childByAutoId() //generate auto id
+//        likesRef.setValue(likesDict)
+//
+//         DataService.usernameRef.child(uid).child("posts").observeEventType(FIRDataEventType.ChildAdded, withBlock:{(snapshot) in
+//                    let postKey = snapshot.key
+        
+//            DataService.postRef.child(postKey).child("UsersWhoLiked").observeSingleEventOfType(.ChildAdded, withBlock: { (likesSnapshot) in
+//        
+//        DataService.
+//            
+//                DataService.postRef.child(postKey).child("UsersWhoLiked").updateChildValues([likesRef.key: true])
+//         })
+//    }
+//    
 
+    }
 }
